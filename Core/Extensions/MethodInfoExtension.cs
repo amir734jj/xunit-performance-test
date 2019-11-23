0080f4bd584b56bb717e778ed6c75810ad9e947d
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-11-23 02:30:19 -0600

    Update MethodInfoExtension.cs

--- Core/Extensions/MethodInfoExtension.cs
+++ Core/Extensions/MethodInfoExtension.cs
@@ -9,13 +9,13 @@ namespace XunitPerformanceTest.Extensions
         {
             var (rt1, rt2) = (first.ReturnType, second.ReturnType);
             var (args1, arg2) = (first.GetParameters(), second.GetParameters());
+            var (n1, n2) = (first.Name, second.Name);
 
             var c1 = rt1 == rt2;
             var c2 = args1.Zip(arg2, (a, b) => (a, b))
                 .Aggregate(true,
                     (accu, tuple) => accu && tuple.Item1.ParameterType == tuple.Item2.ParameterType);
 
-            var (n1, n2) = (first.Name, second.Name);
             var c3 = n1.Contains(n2) || n2.Contains(n1);
 
             return c1 && c2 && c3;
